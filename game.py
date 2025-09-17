8547b0a4a82f8ba8caf5fafc1d44e660d53752bc
Aman <aman.ghazanfar@edu.esiee.fr>
2025-09-17 11:20:31 +0200

    feat: game view transition

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import arcade
 from views.menu_view import MenuView
 from views.scenario_view import ScenarioView
 from views.bd_view import BdView
+from views.game_view import GameView
 import config
 from utils import getWindowSize
 
@@ -9,7 +10,7 @@ def main():
     width, height = getWindowSize.func()
     window = arcade.Window(width, height, config.SCREEN_TITLE, resizable=False)
     arcade.set_background_color((252, 252, 252))
-    menu_view = BdView()
+    menu_view = GameView()
     menu_view.setup()
     window.show_view(menu_view)
     arcade.run()
