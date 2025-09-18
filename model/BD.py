65b13bf02e8fb524f6753b9e0f6c721be892ec13
Aman <aman.ghazanfar@edu.esiee.fr>
2025-09-18 11:03:21 +0200

    feat: corrections

--- model/BD.py
+++ model/BD.py
@@ -1,11 +1,13 @@
 import arcade
 class BD(arcade.Sprite):
     def __init__(self, x, y, image_path, delay, should_be_deleted=False, text=None,clear=False):
-        super().__init__(image_path, scale=0.4)
+        super().__init__(image_path, scale=0.5)
         self.center_x = x
         self.center_y = y
         self.should_be_deleted = should_be_deleted
         self.delay = delay
+        
+            
         self.text = text
         self.clear = clear
 
--- model/MainTitle.py
+++ model/MainTitle.py
@@ -1,7 +1,7 @@
 import arcade
 class MainTitle(arcade.Sprite):
     def __init__(self, x, y):
-        super().__init__("./assets/Sacre_sac.png", scale=0.5)
+        super().__init__("./assets/Sacre_sac.png", scale=0.25)
         self.center_x = x
         self.center_y = y
  
